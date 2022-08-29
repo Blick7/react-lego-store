fc81c47cd5a1335ca4672c3af9384e855ada9ee8
Blick <dima.8m8@gmail.com>
2022-08-29 19:19:29 +0300

    feat: add Starting page

---
+++ src/components/StartingPage/StartingPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import MotionBanner from './MotionBanner';
+import RecommendedCarousel from './RecommendedCarousel';
+
+const StartingPage = () => {
+  return (
+    <div>
+      <MotionBanner />
+      <RecommendedCarousel />
+    </div>
+  );
+};
+
+export default StartingPage;

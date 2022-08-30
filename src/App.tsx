e1df15172c44673cd3285b48465313eac7caeb46
Blick <dima.8m8@gmail.com>
2022-08-30 16:26:43 +0300

    refactor: edit component name

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Layout from './components/Layout/Layout';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import HomeComponent from './components/pages/HomePage';
+import HomePage from './components/pages/HomePage';
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/home" element={<HomeComponent />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="*" element={<Navigate replace to="/home" />}></Route>
       </Routes>
     </Layout>

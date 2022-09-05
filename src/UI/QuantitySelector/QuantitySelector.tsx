ef3fd3544ae0e47fa28cad4cca74b19bc7d020e8
Blick <dima.8m8@gmail.com>
2022-09-05 17:15:05 +0300

    refactor: move quantity selector to a separate component

---
+++ src/UI/QuantitySelector/QuantitySelector.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+
+import classes from './QuantitySelector.module.scss';
+
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
+
+type Props = {
+  value: number;
+};
+
+const QuantitySelector: React.FC<Props> = ({ value }) => {
+  const [inputValue, setInputValue] = useState(value);
+  const [removeBtnActive, setRemoveBtnActive] = useState(false);
+  const [addBtnActive, setAddBtnActive] = useState(true);
+
+  const removeBtnClass = removeBtnActive
+    ? `${classes.button}`
+    : `${classes.button} ${classes['btn-disabled']}`;
+  const addBtnClass = addBtnActive
+    ? `${classes.button}`
+    : `${classes.button} ${classes['btn-disabled']}`;
+
+  const min = 1;
+  const max = 10;
+
+  const removeItemHandler = () => {
+    if (inputValue === min) {
+      setRemoveBtnActive(false);
+      return;
+    }
+    if (inputValue !== null) {
+      setInputValue((prevValue) => prevValue - 1);
+      if (inputValue === min + 1) {
+        setRemoveBtnActive(false);
+      }
+      setAddBtnActive(true);
+    }
+  };
+
+  const addItemHandler = () => {
+    if (inputValue === max) {
+      setAddBtnActive(false);
+      return;
+    }
+    if (inputValue !== null) {
+      setInputValue((prevValue) => prevValue + 1);
+
+      if (inputValue >= max - 1) {
+        setAddBtnActive(false);
+      }
+      setRemoveBtnActive(true);
+    }
+  };
+
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (Number(event.target.value) > max) {
+      setInputValue(max);
+      return;
+    } else if (Number(event.target.value) < min) {
+      setInputValue(min);
+    }
+    setInputValue(Number(event.target.value));
+  };
+
+  const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.target.select();
+  };
+  return (
+    <div className={classes.quantity}>
+      <button className={removeBtnClass} onClick={removeItemHandler}>
+        <RemoveIcon />
+      </button>
+      <input
+        type="number"
+        value={inputValue}
+        onChange={inputChangeHandler}
+        onFocus={handleFocus}
+      />
+      <button className={addBtnClass} onClick={addItemHandler}>
+        <AddIcon />
+      </button>
+    </div>
+  );
+};
+
+export default QuantitySelector;
--- src/components/Shop/Product/ProductOverview.tsx
+++ src/components/Shop/Product/ProductOverview.tsx
@@ -1,13 +1,13 @@
 import React, { useRef, useState } from 'react';
 import DrawStarsRating from '../../../UI/DrawStarsRating';
+import QuantitySelector from '../../../UI/QuantitySelector/QuantitySelector';
 
-import RemoveIcon from '@mui/icons-material/Remove';
-import AddIcon from '@mui/icons-material/Add';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import DescriptionIcon from '@mui/icons-material/Description';
+
 import classes from './ProductOverview.module.scss';
 
 type Props = {
@@ -27,63 +27,7 @@ const ProductOverview: React.FC<Props> = ({
   avaliability,
   id,
 }) => {
-  const [inputValue, setInputValue] = useState(1);
   const featuredClass = featured.length !== 0 ? classes.featured : '';
-  const [removeBtnActive, setRemoveBtnActive] = useState(false);
-  const [addBtnActive, setAddBtnActive] = useState(true);
-
-  const removeBtnClass = removeBtnActive
-    ? `${classes.button}`
-    : `${classes.button} ${classes['btn-disabled']}`;
-  const addBtnClass = addBtnActive
-    ? `${classes.button}`
-    : `${classes.button} ${classes['btn-disabled']}`;
-
-  const min = 1;
-  const max = 10;
-
-  const removeItemHandler = () => {
-    if (inputValue === min) {
-      setRemoveBtnActive(false);
-      return;
-    }
-    if (inputValue !== null) {
-      setInputValue((prevValue) => prevValue - 1);
-      if (inputValue === min + 1) {
-        setRemoveBtnActive(false);
-      }
-      setAddBtnActive(true);
-    }
-  };
-
-  const addItemHandler = () => {
-    if (inputValue === max) {
-      setAddBtnActive(false);
-      return;
-    }
-    if (inputValue !== null) {
-      setInputValue((prevValue) => prevValue + 1);
-
-      if (inputValue >= max - 1) {
-        setAddBtnActive(false);
-      }
-      setRemoveBtnActive(true);
-    }
-  };
-
-  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (Number(event.target.value) > max) {
-      setInputValue(max);
-      return;
-    } else if (Number(event.target.value) < min) {
-      setInputValue(min);
-    }
-    setInputValue(Number(event.target.value));
-  };
-
-  const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
-    event.target.select();
-  };
 
   return (
     <div className={classes.overview}>
@@ -100,20 +44,7 @@ const ProductOverview: React.FC<Props> = ({
       <h3>{title}</h3>
       <div className={classes.price}>${price}</div>
       <span className={classes.avaliability}>{avaliability}</span>
-      <div className={classes.quantity}>
-        <button className={removeBtnClass} onClick={removeItemHandler}>
-          <RemoveIcon />
-        </button>
-        <input
-          type="number"
-          value={inputValue}
-          onChange={inputChangeHandler}
-          onFocus={handleFocus}
-        />
-        <button className={addBtnClass} onClick={addItemHandler}>
-          <AddIcon />
-        </button>
-      </div>
+      <QuantitySelector value={1} />
       <div
         style={{
           display: 'flex',
